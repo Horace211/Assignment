4dfb165b8fd1422f3ab17cd7e4d1368359ae4ad7
✯ нσяαcє sєycнєℓℓ✯ < c syc>
2017-12-08 01:30:28 +0100

    -Background Image Added And Placed -Player Image Added -Player 1 Script Added -Collidors Added and placed -Ball Image Placed

---
+++ Ping pong/Assets/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 20f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
+
